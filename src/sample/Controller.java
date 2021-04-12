c1fc6d67bfc7a4065e1f9658b86b2c18f4d3592a
cyberszot <rene.szotkowski@seznam.cz>
2021-04-12 22:49:49 +0200

    window is not resizable und File/Quit quits app

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+
 public class Controller {
+    @FXML protected void handleQuitButtonAction(ActionEvent event){
+        Platform.exit();
+    }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,12 +12,17 @@ public class Main extends Application  {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root =  FXMLLoader.load(getClass().getClassLoader().getResource("../../data/sample.fxml"));
-        primaryStage.setTitle("hoj");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root =  FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        //Parent root =  FXMLLoader.load(getClass().getClassLoader().getResource("../../data/sample.fxml"));
+        primaryStage.setTitle("IJAProject");
+        primaryStage.setScene(new Scene(root, 640, 420));
         primaryStage.show();
+
+        /*  limits window resizability  */
+        primaryStage.setResizable(false);
+        /*----------------------*/
     }
 
-    public static void main(String[] args) {launch(args); }
+    public static void main(String[] args) { launch(args); }
 
 }
