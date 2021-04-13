6d1dec1a0b5f2196a0f88feb4f40f72d67dd58b4
kiwibojovnik <xkyjov03@vutbr.cz>
2021-04-13 21:10:26 +0200

    Druhý ukol - Dalibor

---
+++ src/sample/StoreGoods.java
@@ -0,0 +1,63 @@
+package sample;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+
+
+public class StoreGoods implements Goods {
+
+    private String name;
+    private HashSet<GoodsItem> items;
+
+
+    public StoreGoods(String name) {
+        this.name = name;
+        this.items = new HashSet<GoodsItem>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StoreGoods goods = (StoreGoods) o;
+        return Objects.equals(name, goods.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public boolean addItem(GoodsItem goodsItem) {
+        return this.items.add(goodsItem);
+    }
+
+    @Override
+    public GoodsItem newItem(LocalDate localDate) {
+        StoreGoodsItem item = new StoreGoodsItem(this, localDate);
+        this.addItem(item);
+        return item;
+    }
+
+    @Override
+    public boolean remove(GoodsItem goodsItem) {
+        return this.items.remove(goodsItem);
+    }
+
+    @Override
+    public boolean empty() {
+        return this.items.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return this.items.size();
+    }
+}
