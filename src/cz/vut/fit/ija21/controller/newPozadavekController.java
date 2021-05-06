a59d7971e7e23a190177dff85957164cb028b2fc
cyberszot <rene.szotkowski@seznam.cz>
2021-05-06 22:36:02 +0200

    chci svuj pozadavek

--- src/cz/vut/fit/ija21/controller/newPozadavekController.java
+++ src/cz/vut/fit/ija21/controller/newPozadavekController.java
@@ -5,18 +5,30 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class newPozadavekController implements Initializable {
 
     @FXML private ListView<String> goodsList;
+    @FXML private Text selectedGoods;
+    @FXML private TextField goodsCount;
+    @FXML private Button finalise;
     ObservableList list = FXCollections.observableArrayList();
+    private int pocet = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Main.nameGoodsRequest.clear();
+        Main.countGoodsRequest.clear();
         load_goods();
     }
 
@@ -29,5 +41,35 @@ public class newPozadavekController implements Initializable {
         }
 
         goodsList.getItems().addAll(list);
+    }
+
+    public void gooodsSelected(MouseEvent mouseEvent) {
+        selectedGoods.setText(goodsList.getSelectionModel().getSelectedItem());
+    }
+
+    public void addPozadavek(MouseEvent mouseEvent) {
+        pocet++;
+        Main.nameGoodsRequest.add(goodsList.getSelectionModel().getSelectedItem());
+        Main.countGoodsRequest.add(goodsCount.getText());
+    }
+
+    public void zpracujPozadavek(MouseEvent mouseEvent) {
+        int[] indexGoodsRequest = new int[pocet];
+        int u = 0;
+        int c = 0;
+        // iterating over an array
+        for (String i : Main.goodsInShelfs) {
+            c++;
+            for (String j : Main.nameGoodsRequest) {
+                if (i.equals(j)) {
+                    indexGoodsRequest[u] = c;
+                    u++;
+                }
+            }
+        }
+        System.out.println(Main.nameGoodsRequest);
+        System.out.println(Arrays.toString(indexGoodsRequest));
+        Stage stage = (Stage) ((Button)mouseEvent.getSource()).getScene().getWindow();
+        stage.close();
     }
 }
