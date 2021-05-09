c35c254b8ca170ca396df8b89fd6e33116e8a85d
kiwibojovnik <xkyjov03@vutbr.cz>
2021-05-09 20:27:54 +0200

    upraven skript a build.xml na odevzdani

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -2,8 +2,8 @@
 #!/bin/sh
 
 #pro mac
-wget -P lib/ https://download2.gluonhq.com/openjfx/16/openjfx-16_osx-x64_bin-sdk.zip
+wget -P lib/ https://download2.gluonhq.com/openjfx/11.0.2/openjfx-11.0.2_linux-x64_bin-sdk.zip
 
-unzip lib/openjfx-16_osx-x64_bin-sdk.zip -d lib/
+unzip lib/openjfx-11.0.2_linux-x64_bin-sdk.zip -d lib/
 
-[ -e lib/openjfx-16_osx-x64_bin-sdk.zip ] && rm lib/openjfx-16_osx-x64_bin-sdk.zip
+[ -e lib/openjfx-11.0.2_linux-x64_bin-sdk.zip ] && rm lib/openjfx-11.0.2_linux-x64_bin-sdk.zip
